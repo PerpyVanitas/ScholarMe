0e56a6cd4c1ddf77cdfac6cc400096086bde2d7c
PerpyVanitas <perpetua.van2@gmail.com>
2026-07-25 00:47:53 +0800

    fix: disable overly strict and invalid eslint rules causing CI failure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,13 +26,8 @@ export default [
       "@typescript-eslint/no-explicit-any": "error",
       "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-unused-vars": "off",
-      "import/no-anonymous-default-export": "off",
-      "react-hooks/exhaustive-deps": "off",
-      "react-hooks/immutability": "off",
-      "react-hooks/preserve-manual-memoization": "warn",
-      "react-hooks/purity": "warn",
       "react-hooks/set-state-in-effect": "off",
-      "react/no-unescaped-entities": "warn",
+      "react-hooks/immutability": "off"
     },
   },
 ];
